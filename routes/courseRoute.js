e47b5620b9799089ee391a9108ac07a8263fc98f
denizdagli <denizdaglice@gmail.com>
2023-05-05 14:49:55 +0300

    Single Course Page Created

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route('/').post(courseController.createCourse);
 router.route('/').get(courseController.getAllCourse);
+router.route('/:slug').get(courseController.getCourse);
 
 module.exports = router;
