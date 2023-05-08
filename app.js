10f35ada7bac5c8aacb33273d9b8daa7019671a4
denizdagli <denizdaglice@gmail.com>
2023-05-08 13:50:47 +0300

    session feature added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
-const userRoute=require('./routes/userRoute')
+const userRoute = require('./routes/userRoute');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 7000;
@@ -13,15 +14,30 @@ mongoose
   .then(() => console.log('Connected!'));
 //template negine
 app.set('view engine', 'ejs');
+//Global Variable
+global.userIN = null;
+
 //middlewares
+
 app.use(express.static('public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard cat',
+    resave: 'false',
+    saveUninitialized: true,
+  })
+);
 //Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
-app.use('/categories',categoryRoute);
-app.use('/users',userRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 app.get('/contact', (req, res) => {
   res.status(200).render('contact', {
