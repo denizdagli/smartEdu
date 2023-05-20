4e891ff9e7cfc2ba47ea1f5ad795e992b4dc9c24
denizdagli <denizdaglice@gmail.com>
2023-05-20 16:54:03 +0300

    Added Update Course

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,6 +7,7 @@ router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.crea
 router.route('/').get(courseController.getAllCourse);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').put(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
 module.exports = router;
